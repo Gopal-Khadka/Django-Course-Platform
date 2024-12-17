0733bf467ab72eaf416b1bc78eceadb2f589a18a
Gopal Khadka <khadkagopal.mail@gmail.com>
2024-12-17 15:49:08 +0545

    Render course image in admin panel

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,4 +1,20 @@
+from cloudinary import CloudinaryImage
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Course
 
-admin.site.register(Course)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["title", "status", "access"]
+    list_filter = ["status", "access"]
+    fields = ["title", "description", "thumbnail", "status", "access", "display_image"]
+    readonly_fields = ["display_image"]
+
+    def display_image(self, instance,*args,**kwargs):
+        url = instance.thumbnail.url
+        cloudinary_id = str(instance.thumbnail)
+        cloudinary_html = CloudinaryImage(cloudinary_id).image(width=500)
+        return format_html(cloudinary_html)
+
+    display_image.short_description = "Current Image"
