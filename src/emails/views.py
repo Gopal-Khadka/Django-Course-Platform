57ae0e7b3a35ffbcbe25edede81f98386ac5d886
Gopal Khadka <khadkagopal.mail@gmail.com>
2024-12-29 09:17:04 +0545

    Use htmx with post data

--- src/emails/views.py
+++ src/emails/views.py
@@ -2,6 +2,8 @@ from django.contrib import messages
 from django.conf import settings
 from django.shortcuts import redirect, render
 from django.http import HttpRequest
+from django_htmx.http import HttpResponseClientRedirect
+
 from . import services
 from .forms import EmailForm
 
@@ -10,14 +12,15 @@ EMAIL_ADDRESS = settings.EMAIL_ADDRESS
 
 def email_token_login_view(request):
     # If request is sent by htmx, then returns the login form
-
     if not request.htmx:
         return redirect("/")
+    email_id_in_session = request.session.get("email_id")
     form = EmailForm(request.POST or None)
     template_name = "emails/hx/email_form.html"
     context = {
         "form": form,
         "message": "",
+        "show_form": not email_id_in_session
     }
     if form.is_valid():
         email_val = form.cleaned_data.get("email")
@@ -26,6 +29,8 @@ def email_token_login_view(request):
         context["message"] = (
             f"Success. Check the verification mail from {EMAIL_ADDRESS} in your inbox."
         )
+        # If form is valid, redirects to welcome page
+        return HttpResponseClientRedirect("/welcome")
     else:
         print(form.errors)
 
